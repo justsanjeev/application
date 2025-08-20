8069ec1692cf234b265d78dd1495a04a07d50cf7
Sanjeev Rohila <82951964+justsanjeev@users.noreply.github.com>
2025-08-20 13:22:27 +0530

    Update common.py

--- logger_log/utils/common.py
+++ logger_log/utils/common.py
@@ -12,11 +12,6 @@ handlers = initialize_logging(
     log=logger, log_name="application.log", timestamp=True, color=True
 )
 
-
-def log_caller() -> None:
-    # Uses
-    logger.debug("Hey I am from common module")
-    logger.debug("Debug message - goes to file only")
-    logger.info("Info message - goes to file only")
-    logger.warning("Warning message - goes to both file and console")
-    logger.error("Error message - goes to both file and console")
+def alllow_access() -> None:
+  logger.debug("Debug from common")
+  logger.info("INFO - from common")
